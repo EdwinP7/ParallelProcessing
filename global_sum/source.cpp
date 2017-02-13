4d3a9f3b490c14c0e03a74a335ecc1288a463158
Edwin P <edwinperea7@gmail.com>
2017-02-12 22:56:53 -0800

    Added synchronization with mutex

--- global_sum/source.cpp
+++ global_sum/source.cpp
@@ -3,6 +3,8 @@
 #include <pthread.h>
 using namespace std;
 
+pthread_mutex_t c_lock = PTHREAD_MUTEX_INITIALIZER;
+
 int thread_count;
 
 void *hello(void* rank);
@@ -17,8 +19,9 @@ int main(int argc, char* argv[]){
 	for(thread_id = 0; thread_id < thread_count; thread_id++)
 		pthread_create(&myThreads[thread_id], NULL, hello, (void*)thread_id);
     
+    pthread_mutex_lock(&c_lock);
     cout << "Hello from the main thread." << endl;
-
+    pthread_mutex_unlock(&c_lock);
     //Wait for threads
     for(thread_id = 0; thread_id < thread_count; thread_id++)
     	pthread_join(myThreads[thread_id], NULL);
@@ -28,6 +31,7 @@ int main(int argc, char* argv[]){
 
 void* hello(void* rank){
 	int my_rank = (long long) rank;
-
+	pthread_mutex_lock(&c_lock);
 	cout << "Hello from thread_" << my_rank << " of " << thread_count << endl;
+	pthread_mutex_unlock(&c_lock);
 }
